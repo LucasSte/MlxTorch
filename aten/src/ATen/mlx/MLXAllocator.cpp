b8889e442b9c695c5c581c80ae16b7f185d63788
Lucas Steuernagel <lucas.tnagel@gmail.com>
2024-08-03 16:03:48 -0300

    Create allocator and configure cmake

--- aten/src/ATen/mlx/MLXAllocator.cpp
+++ aten/src/ATen/mlx/MLXAllocator.cpp
@@ -1,12 +1,31 @@
-#include <c10/core/Allocator.h>
-// TODO: How to find this file?
+#include "MLXAllocator.h"
 #include <mlx/allocator.h>
 
 namespace at::mlx {
-struct TORCH_API MLXAllocator final : public c10::Allocator {
 
-  DataPtr allocate(size_t n) {
+void MLXAllocator::Delete(void* ptr) {
+    if (ptr) {
+      ::mlx::core::allocator::Buffer buf = ::mlx::core::allocator::Buffer{ptr};
+      ::mlx::core::allocator::free(buf);
+    }
+}
 
-  }
-};
+DataPtr MLXAllocator::allocate(size_t n) {
+    ::mlx::core::allocator::Buffer buf = ::mlx::core::allocator::malloc_or_wait(n);
+    return DataPtr{buf.ptr(), buf.ptr(), &Delete, at::Device(at::DeviceType::MLX, 0)};
+}
+
+DeleterFnPtr MLXAllocator::raw_deleter() const {
+  return &Delete;
+}
+
+void MLXAllocator::copy_data(void* dest, const void* src, std::size_t count) const {
+  default_copy_data(dest, src, count);
+}
+
+MLXAllocator getMLXAllocator() {
+  static MLXAllocator allocator;
+  return allocator;
+}
+
 }
---
+++ aten/src/ATen/mlx/MLXAllocator.h
@@ -0,0 +1,16 @@
+#include <c10/core/Allocator.h>
+
+namespace at::mlx {
+
+struct TORCH_API MLXAllocator final : public c10::Allocator {
+ public:
+  DataPtr allocate(size_t n) override;
+  DeleterFnPtr raw_deleter() const override;
+  void copy_data(void * dest, const void * src, std::size_t count) const override;
+
+ private:
+  static void Delete(void *ptr);
+};
+
+MLXAllocator getMLXAllocator();
+} // namespace at::mlx
