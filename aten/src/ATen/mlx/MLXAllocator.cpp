17c1cd29b90574392ee36759fba94d9930b95cfe
Lucas Steuernagel <lucas.tnagel@gmail.com>
2024-08-21 09:19:13 -0300

    Create mlxcpu allocator

--- aten/src/ATen/mlx/MLXAllocator.cpp
+++ aten/src/ATen/mlx/MLXAllocator.cpp
@@ -12,11 +12,11 @@ void MLXAllocator::Delete(void* ptr) {
 
 DataPtr MLXAllocator::allocate(size_t n) {
     ::mlx::core::allocator::Buffer buf = ::mlx::core::allocator::malloc_or_wait(n);
-    return DataPtr{buf.ptr(), buf.ptr(), &Delete, at::Device(at::DeviceType::MLX, 0)};
+    return DataPtr{buf.ptr(), buf.ptr(), &MLXAllocator::Delete, at::Device(at::DeviceType::MLX, 0)};
 }
 
 DeleterFnPtr MLXAllocator::raw_deleter() const {
-  return &Delete;
+  return &MLXAllocator::Delete;
 }
 
 void MLXAllocator::copy_data(void* dest, const void* src, std::size_t count) const {
@@ -26,6 +26,29 @@ void MLXAllocator::copy_data(void* dest, const void* src, std::size_t count) con
 MLXAllocator* getMLXAllocator() {
   static MLXAllocator allocator;
   return &allocator;
+}
+
+void MLXCpuAllocator::Delete(void* ptr) {
+  if (ptr) {
+    uint8_t * ctx = (uint8_t*)ptr - 8;
+    uint64_t original = *(uint64_t*)ctx;
+    ::mlx::core::allocator::Buffer buf = ::mlx::core::allocator::Buffer{(void*) original};
+    ::mlx::core::allocator::free(buf);
+  }
+}
+
+DataPtr MLXCpuAllocator::allocate(size_t n) {
+  // NOTE: I modified mlx to allocate n+8, and return ptr+8 for the raw ptr
+  ::mlx::core::allocator::Buffer buf = ::mlx::core::allocator::malloc_or_wait(n);
+  return DataPtr{buf.raw_ptr(), buf.raw_ptr(), &MLXCpuAllocator::Delete, at::Device(at::DeviceType::MLX, 0)};
+}
+
+DeleterFnPtr MLXCpuAllocator::raw_deleter() const {
+  return &MLXCpuAllocator::Delete;
+}
+
+void MLXCpuAllocator::copy_data(void* dest, const void* src, std::size_t count) const {
+  default_copy_data(dest, src, count);
 }
 
 }
