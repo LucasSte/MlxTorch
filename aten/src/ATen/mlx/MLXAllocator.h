8559183e6cb3f4608c684e304fc71457adfc822b
Lucas Steuernagel <lucas.tnagel@gmail.com>
2024-08-23 09:33:34 -0300

    Use the MLXCPU allcator as the default cpu allocator

--- aten/src/ATen/mlx/MLXAllocator.h
+++ aten/src/ATen/mlx/MLXAllocator.h
@@ -12,16 +12,16 @@ struct TORCH_API MLXAllocator final : public c10::Allocator {
   static void Delete(void *ptr);
 };
 
-struct TORCH_API MLXCpuAllocator final : public c10::Allocator {
- public:
-  DataPtr allocate(size_t n) override;
-  DeleterFnPtr raw_deleter() const override;
-  void copy_data(void * dest, const void * src, std::size_t count) const override;
-
- private:
-  static void Delete(void *ptr);
-};
+//struct TORCH_API MLXCpuAllocator final : public c10::Allocator {
+// public:
+//  DataPtr allocate(size_t n) override;
+//  DeleterFnPtr raw_deleter() const override;
+//  void copy_data(void * dest, const void * src, std::size_t count) const override;
+//
+// private:
+//  static void Delete(void *ptr);
+//};
 
 MLXAllocator* getMLXAllocator();
-MLXCpuAllocator* getMLXCpuAllocator();
+//MLXCpuAllocator* getMLXCpuAllocator();
 } // namespace at::mlx
