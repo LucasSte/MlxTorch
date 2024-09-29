fe48ecd305834358026b611448fbc60898e4e918
Lucas Steuernagel <lucas.tnagel@gmail.com>
2024-09-29 17:09:58 -0300

    Copy to cpu for printing

--- aten/src/ATen/native/mlx/LinearAlgebra.cpp
+++ aten/src/ATen/native/mlx/LinearAlgebra.cpp
@@ -63,13 +63,12 @@ TORCH_IMPL_FUNC(mul_out_mlx)(const Tensor& self, const Tensor& mat2, const Tenso
 
 
 // 1. Fix the mess I created with memory management
-// 2. Check if I can bring methods I shared with MPS to the MLX backend (MPS, MLX and variations in native_functions.yaml)
-// 3. If it works, modify pytorch to not allocate a tensor beforehand for MLX. (register_dispatch_key.py -> create_out)
-// 4. Organize files and remove prints
-// 5. Make sure the minimal example is working.
-// 6. Understand how much work is needed to run the benchmarks
-// 6.1. IF too much, give up and release.
-// 6.2. If quick, benchmark and release.
+// 2. If it works, modify pytorch to not allocate a tensor beforehand for MLX. (register_dispatch_key.py -> create_out)
+// 3. Organize files and remove prints
+// 4. Make sure the minimal example is working (If I use MPS operations, they need syncing, otherwise, only evaluate when copying to cpu).
+// 5. Understand how much work is needed to run the benchmarks
+// 5.1. IF too much, give up and release.
+// 5.2. If quick, benchmark and release.
 }
 
 // TODO: Put the following in another file
