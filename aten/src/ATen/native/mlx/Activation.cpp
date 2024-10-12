1401dc8a397af1b7897cafb63b6e0d02eb6c4ccf
Lucas Steuernagel <lucas.tnagel@gmail.com>
2024-10-12 16:49:40 -0300

    Put sigmoid in activation.cpp

--- aten/src/ATen/native/mlx/Activation.cpp
+++ aten/src/ATen/native/mlx/Activation.cpp
@@ -2,6 +2,13 @@
 #include <ATen/native/mlx/Convert.h>
 #include <mlx/ops.h>
 
+#ifndef AT_PER_OPERATOR_HEADERS
+#include <ATen/Functions.h>
+#include <ATen/NativeFunctions.h>
+#else
+#include <ATen/ops/sigmoid_native.h>
+#endif
+
 namespace at::native {
 Tensor relu_mlx(const Tensor& self) {
   ::mlx::core::array array = mlx::convert::tensor_to_mlx(self);
@@ -11,4 +18,13 @@ Tensor relu_mlx(const Tensor& self) {
 
   return mlx::convert::new_from_mlx(result);
 }
+
+TORCH_IMPL_FUNC(sigmoid_out_mlx)(const Tensor& self, const Tensor& output) {
+  ::mlx::core::array self_mlx = mlx::convert::tensor_to_mlx(self);
+  ::mlx::core::array result_mlx = ::mlx::core::sigmoid(self_mlx, ::mlx::core::Device::gpu);
+  result_mlx.eval();
+
+  mlx::convert::set_tensor_result(result_mlx, output);
+}
+
 }
