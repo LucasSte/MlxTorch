6cba3707de80461b4a6be59a2356659124615a20
Lucas Steuernagel <lucas.tnagel@gmail.com>
2024-10-12 16:44:51 -0300

    Implement bce backward

--- aten/src/ATen/native/mlx/Losses.h
+++ aten/src/ATen/native/mlx/Losses.h
@@ -6,4 +6,9 @@ Tensor binary_cross_entropy_mlx(const Tensor& input,
                                 const Tensor& target,
                                 const std::optional<Tensor>& weight_opt,
                                 int64_t reduction);
+Tensor binary_cross_entropy_backward_mlx(const Tensor& grad_output,
+                                         const Tensor& input,
+                                         const Tensor& target,
+                                         const std::optional<Tensor>& weight_opt,
+                                         int64_t reduction);
 }
