d978ca3c512ec038e854052cba090594e5c7dd2f
Lucas Steuernagel <lucas.tnagel@gmail.com>
2024-10-13 15:42:43 -0300

    Start implementing sum out

--- aten/src/ATen/native/mlx/Convert.h
+++ aten/src/ATen/native/mlx/Convert.h
@@ -5,6 +5,7 @@
 
 namespace at::native::mlx::convert {
 ::mlx::core::Dtype convert_type(const Tensor &self);
+::mlx::core::Dtype convert_scalar_type(ScalarType t);
 ::mlx::core::array tensor_to_mlx(const Tensor &self);
 void set_tensor_result(const ::mlx::core::array & mlx_result, const Tensor & tensor_result, const std::string name = __builtin_FUNCTION());
 Tensor new_from_mlx(const ::mlx::core::array & input);
