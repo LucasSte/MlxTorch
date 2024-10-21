807903fdb9711bf53e933987a58fa2ff574d59c2
Lucas Steuernagel <lucas.tnagel@gmail.com>
2024-10-21 12:07:05 -0300

    Implement addcmul

---
+++ aten/src/ATen/native/mlx/PointWiseOps.cpp
@@ -0,0 +1,36 @@
+#define TORCH_ASSERT_ONLY_METHOD_OPERATORS
+#include <ATen/TensorMeta.h>
+#include <ATen/Scalar.h>
+#include <mlx/array.h>
+#include <mlx/ops.h>
+#include <ATen/native/mlx/Convert.h>
+
+#ifndef AT_PER_OPERATOR_HEADERS
+#include <ATen/Functions.h>
+#include <ATen/NativeFunctions.h>
+#else
+#include <ATen/ops/addcmul_native.h>
+#endif
+
+namespace at::native {
+TORCH_IMPL_FUNC(addcmul_out_mlx)
+(const Tensor& self,
+ const Tensor& tensor1,
+ const Tensor& tensor2,
+ const Scalar& value,
+ const Tensor& output) {
+
+  ::mlx::core::array t1 = mlx::convert::tensor_to_mlx(tensor1);
+  ::mlx::core::array t2 = mlx::convert::tensor_to_mlx(tensor2);
+  ::mlx::core::array val = ::mlx::core::array(static_cast<float>(value.toDouble()), mlx::convert::convert_type(tensor1));
+
+  ::mlx::core::array mul1 = ::mlx::core::multiply(t1, t2, ::mlx::core::Device::gpu);
+  ::mlx::core::array mul2 = ::mlx::core::multiply(val, mul1, ::mlx::core::Device::gpu);
+  ::mlx::core::array self_mlx = mlx::convert::tensor_to_mlx(self);
+
+  ::mlx::core::array result_mlx = ::mlx::core::add(self_mlx, mul2, ::mlx::core::Device::gpu);
+  result_mlx.eval();
+
+  mlx::convert::set_tensor_result(result_mlx, output);
+}
+}
