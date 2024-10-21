409e922e28f2411580c28fe47955fbc53d16a02a
Lucas Steuernagel <lucas.tnagel@gmail.com>
2024-10-21 19:22:44 -0300

    Update todo

--- aten/src/ATen/native/mlx/LinearAlgebra.cpp
+++ aten/src/ATen/native/mlx/LinearAlgebra.cpp
@@ -64,7 +64,6 @@ TORCH_IMPL_FUNC(mul_out_mlx)(const Tensor& self, const Tensor& mat2, const Tenso
 
 
 // 3. Make sure the minimal example is working (If I use MPS operations, they need syncing, otherwise, only evaluate when copying to cpu).
-// 4. Remove unused prints
 // 5. If it works, modify pytorch to not allocate a tensor beforehand for MLX. (register_dispatch_key.py -> create_out)
 // 6. Only evaluate when needed. If I use MPS operations (try to avoid them), evaluate MLX and sync MPS.
 // 7. Understand how much work is needed to benchmark things (only do it if it is going to be quick)
