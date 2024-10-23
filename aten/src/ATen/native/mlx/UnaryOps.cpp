7ea4f3d050ce9038d6c2d7c1837ebe0f5df6c64c
Lucas Steuernagel <lucas.tnagel@gmail.com>
2024-10-23 11:37:55 -0300

    Implement round out

--- aten/src/ATen/native/mlx/UnaryOps.cpp
+++ aten/src/ATen/native/mlx/UnaryOps.cpp
@@ -10,6 +10,7 @@
 #include <ATen/NativeFunctions.h>
 #else
 #include <ATen/ops/sqrt_native.h>
+#include <ATen/ops/round_native.h>
 #endif
 
 
@@ -17,6 +18,13 @@ namespace at::native {
 TORCH_IMPL_FUNC(sqrt_out_mlx)(const Tensor& self, const Tensor& output) {
   ::mlx::core::array self_mlx = mlx::convert::tensor_to_mlx(self);
   ::mlx::core::array result = ::mlx::core::sqrt(self_mlx, ::mlx::core::Device::gpu);
+  result.eval();
+  mlx::convert::set_tensor_result(result, output);
+}
+
+TORCH_IMPL_FUNC(round_out_mlx)(const Tensor& self, const Tensor& output) {
+  ::mlx::core::array self_mlx = mlx::convert::tensor_to_mlx(self);
+  ::mlx::core::array result = ::mlx::core::round(self_mlx, 0, ::mlx::core::Device::gpu);
   result.eval();
   mlx::convert::set_tensor_result(result, output);
 }
