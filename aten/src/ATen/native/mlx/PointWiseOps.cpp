3562e027942014b1b0e51eeeaa553b5e022352b9
Lucas Steuernagel <lucas.tnagel@gmail.com>
2024-10-23 07:15:49 -0300

    Implement addcdiv

--- aten/src/ATen/native/mlx/PointWiseOps.cpp
+++ aten/src/ATen/native/mlx/PointWiseOps.cpp
@@ -9,6 +9,7 @@
 #include <ATen/Functions.h>
 #include <ATen/NativeFunctions.h>
 #else
+#include <ATen/ops/addcdiv_native.h>
 #include <ATen/ops/addcmul_native.h>
 #endif
 
@@ -29,6 +30,26 @@ TORCH_IMPL_FUNC(addcmul_out_mlx)
   ::mlx::core::array self_mlx = mlx::convert::tensor_to_mlx(self);
 
   ::mlx::core::array result_mlx = ::mlx::core::add(self_mlx, mul2, ::mlx::core::Device::gpu);
+  result_mlx.eval();
+
+  mlx::convert::set_tensor_result(result_mlx, output);
+}
+
+TORCH_IMPL_FUNC(addcdiv_out_mlx)
+(const Tensor& self,
+ const Tensor& tensor1,
+ const Tensor& tensor2,
+ const Scalar& value,
+ const Tensor& output) {
+  ::mlx::core::array t1 = mlx::convert::tensor_to_mlx(tensor1);
+  ::mlx::core::array t2 = mlx::convert::tensor_to_mlx(tensor2);
+  ::mlx::core::array val = mlx::convert::scalar_to_mlx(value);
+
+  ::mlx::core::array div = ::mlx::core::divide(t1, t2, ::mlx::core::Device::gpu);
+  ::mlx::core::array mul = ::mlx::core::multiply(val, div, ::mlx::core::Device::gpu);
+  ::mlx::core::array self_mlx = mlx::convert::tensor_to_mlx(self);
+
+  ::mlx::core::array result_mlx = ::mlx::core::add(self_mlx, mul, ::mlx::core::Device::gpu);
   result_mlx.eval();
 
   mlx::convert::set_tensor_result(result_mlx, output);
