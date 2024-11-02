aa6218dc1898da5eeaa467885f153b32274ffa13
Lucas Steuernagel <lucas.tnagel@gmail.com>
2024-11-02 20:16:15 -0300

    Minimal example is working

--- aten/src/ATen/native/mlx/LinearAlgebra.cpp
+++ aten/src/ATen/native/mlx/LinearAlgebra.cpp
@@ -62,12 +62,13 @@ TORCH_IMPL_FUNC(mul_out_mlx)(const Tensor& self, const Tensor& mat2, const Tenso
 //  }
 //
 
-// 1. Make sure the minimal example is working (If I use MPS operations, they need syncing, otherwise, only evaluate when copying to cpu).
-// 2. Fix the type bug in the minimal example
+// 1. Fix the type bug in the minimal example
+// 2. Ensure all operations are correctly implemented (write mlx evaluation tests)
 // 3. If it works, modify pytorch to not allocate a tensor beforehand for MLX. (register_dispatch_key.py -> create_out)
-// 4. Only evaluate when needed. If I use MPS operations (try to avoid them), evaluate MLX and sync MPS.
-// 5. Understand how much work is needed to benchmark things (only do it if it is going to be quick)
-// 6. Release
+// 4. Reduce operations overhead (creating mlx tensors)
+// 5. Only evaluate when needed. If I use MPS operations (try to avoid them), evaluate MLX and sync MPS.
+// 6. Understand how much work is needed to benchmark things (only do it if it is going to be quick)
+// 7. Release
 }
 
 TORCH_IMPL_FUNC(addmm_out_mlx)
--- aten/src/ATen/native/mlx/TensorFactory.cpp
+++ aten/src/ATen/native/mlx/TensorFactory.cpp
@@ -3,6 +3,9 @@
 #include <ATen/Tensor.h>
 #include <ATen/Utils.h>
 #include <ATen/mlx/EmptyTensor.h>
+#include <mlx/allocator.h>
+#include <ATen/native/ResizeCommon.h>
+#include <ATen/native/Resize.h>
 
 namespace at::native {
 
@@ -29,50 +32,65 @@ Tensor empty_strided_mlx(
   return result;
 }
 
-//const Tensor & resize_mps_(
-//    const Tensor &self,
-//    IntArrayRef size,
-//    std::optional<MemoryFormat> optional_memory_format
-//    ) {
-//  auto* self_ = self.unsafeGetTensorImpl();
-//  int64_t old_storage_nbytes = self_->unsafe_storage() ? self_->unsafe_storage().nbytes() : 0;
-//
-//  if (self_->sizes() == size) {
-//    return self;
-//  }
-//  self_->set_sizes_contiguous(size);
-//  int64_t storage_size = self_->numel();
-//
-//  auto storage = self_->storage().unsafeGetStorageImpl();
-//  uint64_t new_size_bytes = (storage_size + self_->storage_offset()) * self_->dtype().itemsize();
-//
-//  if (new_size_bytes > self_->storage().nbytes()) {
-//    at::DataPtr new_data = storage->allocator()->allocate(new_size_bytes);
-//    size_t copy_capacity = std::min<size_t>(new_size_bytes, storage->nbytes());
-//
-//    if (storage->data() && copy_capacity > 0) {
-//      // TODO: This isn't right
-//      at::native::mps::copy_blit_mps(new_data.get(), storage->data(), copy_capacity);
-//    }
-//    // Destructively overwrite data_ptr
-//    storage->set_data_ptr_noswap(std::move(new_data));
-//    storage->set_nbytes(new_size_bytes);
-//  }
-//
-//  if (optional_memory_format.has_value()) {
-//    auto memory_format =
-//        optional_memory_format.value();
-//    TORCH_CHECK(
-//        memory_format != MemoryFormat::Preserve,
-//        "Unsupported memory format",
-//        memory_format);
-//    self_->empty_tensor_restride(memory_format);
-//  }
-//  if (C10_UNLIKELY(at::globalContext().deterministicAlgorithms() && at::globalContext().deterministicFillUninitializedMemory())) {
-//    at::native::fill_resize_deterministic_(self, old_storage_nbytes);
-//  }
-//  return self;
-//}
+inline static void maybe_resize_storage_mlx(TensorImpl * self, uint64_t new_size) {
+  if (new_size == 0) {
+    return;
+  }
+
+  auto storage = self->storage().unsafeGetStorageImpl();
+  if (!storage) {
+    TORCH_CHECK(false, "Tensor: invalid null storage");
+  }
+  uint64_t new_size_bytes = (new_size + self->storage_offset()) * self->dtype().itemsize();
+  if (new_size_bytes > self->storage().nbytes()) {
+    at::DataPtr new_data = storage->allocator()->allocate(new_size_bytes);
+    size_t copy_capacity = std::min<size_t>(new_size_bytes, storage->nbytes());
+    if (storage->data() && copy_capacity > 0) {
+      ::mlx::core::allocator::MemControl * old_ctrl = ::mlx::core::allocator::MemControl::mem_control_ptr(const_cast<void*>(storage->data()));
+      ::mlx::core::allocator::Buffer old_buf = {old_ctrl->mtl_ptr};
+      ::mlx::core::allocator::MemControl * new_ctrl = ::mlx::core::allocator::MemControl::mem_control_ptr(new_data.get());
+      ::mlx::core::allocator::Buffer new_buf = {new_ctrl->mtl_ptr};
+      // TODO: Is this the best way to copy?
+      std::memcpy(new_buf.raw_ptr(), old_buf.raw_ptr(), copy_capacity);
+    }
+    // Destructively overwrite data_ptr
+    storage->set_data_ptr_noswap(std::move(new_data));
+    storage->set_nbytes(new_size_bytes);
+  }
+}
+
+inline TensorImpl * resize_impl_mlx_(
+    TensorImpl* self,
+    IntArrayRef size,
+    std::optional<IntArrayRef> stride,
+    bool device_guard = true) {
+  if (self->sizes() == size && (!stride || self->strides() == stride)) {
+    return self;
+  }
+
+  int64_t storage_size = 1;
+  if (stride) {
+    self->set_sizes_and_strides(size, *stride);
+    // NB: storage size can be different from numel.
+    storage_size = storage_size_for(size, *stride);
+  } else {
+    self->set_sizes_contiguous(size);
+    storage_size = self->numel();
+  }
+  maybe_resize_storage_mlx(self, storage_size);
+
+  return self;
+}
+
+Tensor& set_storage_mlx_(Tensor& result, Storage storage, int64_t storage_offset, IntArrayRef size, IntArrayRef stride) {
+  checkSetStorage(result, std::move(storage), storage_offset, size, stride);
+  //std::cout << "set storage_mps " << storage_offset << " stride " << stride << std::endl;
+  result.unsafeGetTensorImpl()->set_storage_offset(storage_offset);
+  std::optional<IntArrayRef> stride_opt = stride.data() != nullptr ?
+                                                                   std::optional<IntArrayRef>(stride) : std::nullopt;
+  resize_impl_mlx_(result.unsafeGetTensorImpl(), size, stride_opt);
+  return result;
+}
 
 }
 
